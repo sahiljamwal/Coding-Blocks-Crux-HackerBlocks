53e6bc50c68fde73999b2ec5c0bf1973d077e78c
Sahil Jamwal <sahil.jamwal78625@gmail.com>
2020-05-18 19:40:26 +0530

    addedd pattern questions

---
+++ Fundamentals01/Pattern9.java
@@ -0,0 +1,35 @@
+package Fundamentals01;
+
+import java.util.Scanner;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int nst = 1;
+		int nsp = n - 1;
+
+		int row = 1;
+		while (row <= n) {
+			// space
+			for (int csp = 1; csp <= nsp; csp++) {
+				System.out.print(" ");
+			}
+
+			// star
+			for (int cst = 1; cst <= nst; cst++) {
+				System.out.print("*");
+			}
+
+			// prepare
+			System.out.println();
+			nst += 2;
+			nsp--;
+			row++;
+		}
+
+		s.close();
+	}
+
+}
